7af8ef285ea0e3322ab697b6518c17077528eff2
Nickneim <nicolasmontes@gmail.com>
2025-01-25 09:46:36 -0300

    add in_inventory paramater to new_bubble

--- bubblecoin/Scenes/Bubble/bubble.gd
+++ bubblecoin/Scenes/Bubble/bubble.gd
@@ -5,39 +5,35 @@ const bubbleScene = preload("res://Scenes/Bubble/bubble.tscn")
 
 @export var bubble_data : BubbleData
 
-var in_inventory = false
-var buy_price
-var sell_price
-
-static func new_bubble(_bubbleData : BubbleData) -> Bubble:
+static func new_bubble(_bubbleData : BubbleData, in_inventory: bool) -> Bubble:
 	var bubble: Bubble = bubbleScene.instantiate()
-	bubble.bubble_data = _bubbleData
-	bubble.set_buy_price(_bubbleData.init_buy_price)
-	bubble.set_sell_price(_bubbleData.init_sell_price)
-	#$SellPrice.text = str(sell_price).pad_decimals(2)
-	#$BuyPrice.text = str(buy_price).pad_decimals(2)
-	bubble.set_name_description(_bubbleData.bubble_name,_bubbleData.modifier_description) 
+	bubble.set_bubble_data(_bubbleData)
+	bubble.set_in_inventory(in_inventory)
 	return bubble
 
+func set_in_inventory(in_inventory: bool) -> void:
+	$BuyBtn.visible = not in_inventory
+	$SellBtn.visible = in_inventory
+
+func set_bubble_data(bubbleData : BubbleData):
+	bubble_data = bubbleData
+	set_buy_price(bubbleData.init_buy_price)
+	set_sell_price(bubbleData.init_sell_price)
+	set_name_description(bubbleData.bubble_name, bubbleData.modifier_description)
+
 func set_buy_price(price):
-	buy_price = price
-	$BuyBtn/BuyPrice.text = "Buy price: ฿ %.2f" % buy_price
+	$BuyBtn/BuyPrice.text = "Buy price: ฿ %.2f" % price
+
+func set_sell_price(price):
+	$SellBtn/SellPrice.text = "Sell price: ฿ %.2f" % price
 
 func set_name_description(_name, description):
 	$name.text = _name
 	$ModifierDescription.text = description
 
-func set_sell_price(price):
-	sell_price = price
-	$SellBtn/SellPrice.text = "Sell price: ฿ %.2f" % sell_price
-
 func buy() -> void:
-	in_inventory = true
-	$SellBtn/SellPrice.visible = not in_inventory
-	$BuyBtn/BuyPrice.visible = in_inventory
 	PlayerVariables.AddBubble(bubble_data)
 	queue_free()
-	
 
 func sell() -> void:
 	PlayerVariables.RemoveBubble(bubble_data)
@@ -46,9 +42,3 @@ func sell() -> void:
 func apply_modifier(counter):
 	counter.multiplier += bubble_data.multiplier
 	counter.addition +=  bubble_data.addition
-
-func set_bubble_data(bubbleData : BubbleData):
-	set_buy_price(bubbleData.init_buy_price)
-	set_sell_price(bubbleData.init_sell_price)
-	$name.text = bubbleData.bubble_name
-	$ModifierDescription.text = bubbleData.modifier_description
