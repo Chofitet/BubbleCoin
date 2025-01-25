1cb3a5282a46ca15f69bc7869eba54fbca84393e
Sofía Tetzlaff <sofiatetzlaff@gmail.com>
2025-01-25 19:38:44 -0300

    muchas cosas

--- bubblecoin/Scenes/Bubble/Bonificadores.gd
+++ bubblecoin/Scenes/Bubble/Bonificadores.gd
@@ -3,8 +3,8 @@ class_name Modificador
 
 @export var ModifyDescription : String
 
-@export var MultiplicadorIdle  : int = 1
-@export var AdicionadorIdle : int = 1
+@export var MultiplicadorIdle  : int = 0
+@export var AdicionadorIdle : int = 0
 
-@export var MultiplicadorClick : int = 1
-@export var AdicionadorClick : int = 1
+@export var MultiplicadorClick : int = 0
+@export var AdicionadorClick : int = 0
