268fd88405b468325845d1591f60bcb5d211d7b8
Nickneim <nicolasmontes@gmail.com>
2025-01-26 01:55:59 -0300

    beginnings of control sizing

--- bubblecoin/Scenes/Web_Window/WebSpawner.gd
+++ bubblecoin/Scenes/Web_Window/WebSpawner.gd
@@ -12,5 +12,13 @@ func SetWebWindow(web : PackedScene):
 
 	
 	var instance_web = web.instantiate()
+	if instance_web.has_signal("ArticleSet"):
+		instance_web.ArticleSet.connect(open_article)
 	add_child(instance_web)
 	WebsPanel.append(instance_web)
+
+
+signal ArticleSet(article_to_open: int)
+
+func open_article(article_to_open):
+	ArticleSet.emit(article_to_open)
