7bf671558db82fdf24edc4741836ac5b74f98db1
Nickneim <nicolasmontes@gmail.com>
2025-02-01 19:32:19 -0300

    clean up clicker and remove focus rectangle

--- bubblecoin/Scenes/Clicker/ContadorCoins.gd
+++ bubblecoin/Scenes/Clicker/ContadorCoins.gd
@@ -1,38 +1,27 @@
 extends Control
 
-@export var label : Label
 @export var btnClick : Texture
 @export var btnIdle : Texture
 
-func _ready() -> void:
-	$TextureRect/Clicker.pressed.connect(_on_clicker_click)
-
-func _process(delta: float) -> void:
-	set_label()
-
 func obtener_coins(valor : float):
 	PlayerVariables.BubbleCoins += valor
-	set_label()
 	
 func gastar_coins(valor : float):
 	PlayerVariables.BubbleCoins -= valor
-	set_label()
 
 func hay_suficientes_bubbles(valor : float) -> bool: 
 	return valor >= PlayerVariables.BubbleCoins
- 
-func _on_clicker_click() -> void:
+
+func click_down() -> void:
 	print("click")
 	var coins_por_click = PlayerVariables.AdicionClicker * PlayerVariables.MultiplicadorClicker
 	obtener_coins(coins_por_click)
 	$TextureRect.texture = btnClick
-	await get_tree().create_timer(0.2).timeout
+
+func click_up() -> void:
+	print("unclick")
 	$TextureRect.texture = btnIdle
 
-func _on_burbujero_generar_burbujas() -> void:
+func burbujero_timeout() -> void:
 	var coins_por_bubujero = PlayerVariables.AdicionIdle * PlayerVariables.MultiplicadorIdle
 	obtener_coins(coins_por_bubujero)
-
-func set_label():
-	if label:
-		label.text = "%.2f" % PlayerVariables.BubbleCoins
